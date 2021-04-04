e9ac7a1a62074a5ccf29f1030448b050ff0a6d72
v.shivanandhareddy <75928849+shivanandhareddy@users.noreply.github.com>
2021-04-04 18:11:08 +0530

    Add files via upload

---
+++ qrgenerator.py
@@ -0,0 +1,14 @@
+#pip install qrcode
+
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,box_size=10,border=5)
+
+data=input("enter data to display on QR code:\n")
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save("gen.png")
+img = Image.open('gen.png')
+img.show()
